fce66a7fdd3da1c4f17ffba5a32b1640d75fd646
Nyein Chan Soe <nyeinchansoe.business@gmail.com>
2024-07-24 13:30:52 +0700

    Minor fix footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,7 +3,7 @@ import Heading1 from "./components/Heading1";
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto px-[50px] flex justify-between items-center bg-primary py-[16px]">
+    <footer className="mx-auto px-[50px] w-full flex justify-between items-center bg-primary py-[16px]">
       <Heading1 text="Contact" color="background" />
       <div className="flex items-center gap-x-12">
         <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer">
