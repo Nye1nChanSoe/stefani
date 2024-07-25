a2fa80fbfc6ea99d31fcd3b1a83ba660c0ed562c
Nyein Chan Soe <nyeinchansoe.business@gmail.com>
2024-07-25 13:34:28 +0700

    Minor Fix Footer container

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,18 +3,20 @@ import Heading1 from "./components/Heading1";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto px-[50px] w-full flex justify-between items-center bg-primary py-[16px]">
-      <Heading1 text="Contact" color="background" />
-      <div className="flex items-center gap-x-12">
-        <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer transition-colors duration-500 hover:text-background">
-          Whatsapp
-        </p>
-        <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer transition-colors duration-500 hover:text-background">
-          Instagram
-        </p>
-        <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer transition-colors duration-500 hover:text-background">
-          LinkedIn
-        </p>
+    <footer className="px-[50px] w-full bg-primary py-[16px]">
+      <div className="mx-auto container flex justify-between items-center">
+        <Heading1 text="Contact" color="background" />
+        <div className="flex items-center gap-x-12">
+          <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer transition-colors duration-500 hover:text-background">
+            Whatsapp
+          </p>
+          <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer transition-colors duration-500 hover:text-background">
+            Instagram
+          </p>
+          <p className="text-[30px] font-open-sans font-bold leading-8 cursor-pointer transition-colors duration-500 hover:text-background">
+            LinkedIn
+          </p>
+        </div>
       </div>
     </footer>
   );
